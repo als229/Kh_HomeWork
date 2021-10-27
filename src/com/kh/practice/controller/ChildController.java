646585d3af47320920bc4fb8eb37cd9c057f255a
als229 <rhksalsdldia@naver.com>
2021-10-28 01:35:31 +0900

    상속 과제

---
+++ src/com/kh/practice/controller/ChildController.java
@@ -0,0 +1,12 @@
+package com.kh.practice.controller;
+
+public class ChildController {
+
+	
+	public int calcSum(int x, int y, int z) {
+		
+	}
+	public int calcSub(int x, int y, int z) {
+		
+	}
+}
---
+++ src/com/kh/practice/view/MainMenu.java
@@ -0,0 +1,16 @@
+package com.kh.practice.view;
+
+public class MainMenu {
+
+	public void mainMenu() {
+		
+	}
+	public void sum() {
+		
+	}
+	
+	public void times() {
+		
+	}
+
+}
